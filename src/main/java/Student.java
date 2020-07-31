a8d6bdfb1177b27fd76f4199b8ad3a8256c80140
Esai Flores <flores.esaijonathan@gmail.com>
2020-07-31 18:31:06 -0500

    finising up test for student class

---
+++ src/main/java/Student.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getId() {
+        return this.id;
+    }
+}
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -16,4 +16,25 @@ public class StudentTest {
         assertSame("esai", esai.getName());
         assertSame(0, esai.getGrades().size());
     }
+
+
+    @Test
+    public void testAddGrade(){
+        Student esai = new Student(1L, "esai");
+        esai.addGrade(100);
+        assertSame(100, esai.getGrades().get(0));
+        esai.addGrade(80);
+        assertSame(80, esai.getGrades().get(1));
+    }
+
+    @Test
+    public void testAverageGrade(){
+        Student esai = new Student(1L, "esai");
+        esai.addGrade(100);
+        esai.addGrade(80);
+        assertEquals(90, esai.getGradeAverage(), 0);
+    }
+
+
+
 }
