2e170048257e11eac2b56f22426639d6ca7dee01
Esai Flores <flores.esaijonathan@gmail.com>
2020-07-31 18:23:49 -0500

    setting up test for student name,id,and grades

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -8,4 +8,12 @@ public class StudentTest {
         assertNull (adam);
         assertNotNull(esai);
     }
+
+    @Test
+    public void testStudentFields(){
+        Student esai = new Student (1L, "esai");
+        assertSame(1L, esai.getId());
+        assertSame("esai", esai.getName());
+        assertSame(0, esai.getGrades().size());
+    }
 }
